ab8afe3f5da7e093bc96d943b349631b37b111a3
kelsie kidd <kelsiekidd93@gmail.com>
2022-09-12 16:36:37 -0500

    removed folders/files. Simplified with help from tutor

---
+++ routes/apiRoutes.js
@@ -0,0 +1,30 @@
+const { writeFile } = require('fs');
+let store = require('../db/db.json');
+const { v1: uuidv1 } = require('uuid');
+const { notStrictEqual } = require('assert');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.json(store);
+});
+
+router.post('/notes', ({body},res)=> {
+    body.id = uuidv1();
+    store.push(body);
+
+    console.log('Store: ',store);
+    writeFile('./db/db.json', JSON.stringify(store), err=>{
+        if(err){console.log(err)};
+        res.json(store);
+    });
+
+})
+
+router.delete('/notes/:id', ({body}, res) => {
+    getStore()
+    .removeId({id})
+    .then(() => res.json({ok: true}))
+    })
+    // console.log(err)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
+const path = require('path');
 const express = require('express');
-const homeRoutes = require('./routes/htmlRoutes/homeRoutes');
-const dbRoutes = require('./routes/apiRoutes/dbRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static(path,join(__dirname, 'public')));
-app.use(require('./api', dbRoutes));
-app.use(require('/', homeRoutes));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./routes'));
 
 app.listen(PORT,()=>console.log(`Now listeing on http://localhost:${PORT}`));
